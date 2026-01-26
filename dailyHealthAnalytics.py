d526b9977bcbae3be5aa8a4aa5960a822fe34084
iamsarath1986 <iamsarath1986@gmail.com>
2026-01-26 16:47:21 +0400

    Add script to calculate mean of steps, workout minutes, and sleep hours within a day range

--- dailyHealthAnalytics.py
+++ dailyHealthAnalytics.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-np.set_printoptions(precision=2, suppress=True, formatter={'float': '{:.2f}'.format})
+np.set_printoptions(precision=2, suppress=True)
 
 array_labels = np.array(["Day", "Steps", "Workout_Minutes", "Avg_Heart_Rate", "Sleep_Hours", "Bodyweight_kg"])
 
@@ -21,31 +21,79 @@ array_data = np.array([
     [9, 7300, 30, 116, 6.0, 81.0]
 ])
 
-day = 14
+print("1. Enter the day number to get the details")
+print("2. Check the average Steps, Workout minutes, Sleep hours")
+try:
+    choice = int(input("Enter your choice: "))
+    if choice not in [1, 2]:
+        print("Number should be 1 or 2.")
+        exit(1)
 
-if not (1 <= day <= array_data.shape[0]):
-    print(f"Day {day} is not valid.")
-    exit(1)
+    label_map = {label: i for i, label in enumerate(array_labels)}
+    day_col = label_map['Day']
+    steps = label_map['Steps']
+    workout_minutes = label_map['Workout_Minutes']
+    sleep_hours = label_map['Sleep_Hours']
 
-label_map = {label: i for i, label in enumerate(array_labels)}
-day_col = label_map['Day']
-mask = array_data[:, day_col] == day
+    if choice == 1:
+        try:
+            day = int(input("Enter the day number: "))
 
-day_value = array_data[mask, label_map['Day']]
-step_value = array_data[mask, label_map['Steps']]
+            if not (1 <= day <= array_data.shape[0]):
+                print(f"Day {day} is not valid.")
+                exit(1)
 
-print("")
-print("1. Basic statistics & slicing")
-print("")
-print("Calculate the mean of Steps, Workout minutes, Sleep hours:")
-print("")
-means = array_data.mean(axis=0)
+            mask = array_data[:, day_col] == day
+            day_value = array_data[mask, label_map['Day']]
+            step_value = array_data[mask, label_map['Steps']]
 
-print(f"{array_labels[1]}: {means[1]:.2f}")
-print(f"{array_labels[2]}: {means[2]:.2f}")
-print(f"{array_labels[4]}: {means[4]:.2f}")
+            print(f"On {array_labels[label_map['Day']].lower()} {int(day_value[0])}, you walked {int(step_value[0])}"
+                  f" {array_labels[label_map['Steps']].lower()}")
+        except ValueError:
+            print("Invalid input.")
 
-'''
-print(f"On {array_labels[label_map['Day']].lower()} {int(day_value[0])}, you walked {int(step_value[0])}"
-      f" {array_labels[label_map['Steps']].lower()}")
-'''
+    elif choice == 2:
+        try:
+            range_days_input = input("Enter the days range (format of the input (8-14, 8 – 14, 8 to 14)): ")
+
+            range_days_replace = range_days_input.replace(" ", "")
+
+            if "to" in range_days_replace:
+                range_days_split = range_days_replace.split('to')
+            elif "-" in range_days_replace:
+                range_days_split = range_days_replace.split('-')
+            else:
+                print("Invalid range.")
+                exit(1) #TODO: Reprompt user for valid input.
+
+            range_begin = int(range_days_split[0])
+            range_end = int(range_days_split[1])
+
+            if range_begin < 1 or range_end < 1:
+                print("Numbers should be greater than zero.")
+                exit(1) #TODO: Reprompt user for valid input.
+
+            if range_begin > range_end:
+                print("Invalid range.")
+                exit(1)
+
+            mask = (array_data[:, day_col] >= range_begin) & (array_data[:, day_col] <= range_end)
+            rows = array_data[mask]
+
+            print("")
+            print("1. Basic statistics & slicing")
+            print("")
+            print("Average (Steps, Workout minutes, Sleep hours):")
+            print("")
+
+            means = rows.mean(axis=0)
+            print(f"{array_labels[steps]}: {means[steps]:.2f}")
+            print(f"{array_labels[workout_minutes]}: {means[workout_minutes]:.2f}")
+            print(f"{array_labels[sleep_hours]}: {means[sleep_hours]:.2f}")
+        except ValueError:
+            print("Invalid input.")
+    else:
+        exit(1)
+
+except ValueError:
+    print("Invalid choice.")
